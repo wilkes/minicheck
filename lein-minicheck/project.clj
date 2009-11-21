bd472929aaeec4e45f06374dbe24e2897f0e3a6b
wilkes <wilkesjoiner@gmail.com>
2009-11-20 22:01:31 -0600

    added lien-push for clojars

--- lein-minicheck/project.clj
+++ lein-minicheck/project.clj
@@ -1,3 +1,4 @@
 (defproject lein-minicheck "0.1.0"
   :dependencies [[leiningen "0.5.0"]
-                 [minicheck "0.1.0"]])
+                 [minicheck "0.1.0"]
+                 [lein-clojars "0.5.0-SNAPSHOT"]])
--- project.clj
+++ project.clj
@@ -1,2 +1,3 @@
 (defproject minicheck "0.1.0"
-  :dev-dependencies [[lein-minicheck "0.1.0"]])
+  :dev-dependencies [[lein-minicheck "0.1.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
