906dfde6743cd0d8e7d724648e7248770672e841
wilkes <wilkesjoiner@gmail.com>
2010-03-28 13:15:56 -0500

    removed lein-minicheck plugin, no longer needed since we use clojure.test

--- lein-minicheck/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject lein-minicheck "0.2.0"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [leiningen "0.5.0"]
-                 [minicheck "0.2.0"]
-                 [lein-clojars "0.5.0-SNAPSHOT"]])
